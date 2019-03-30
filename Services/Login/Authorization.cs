46d21f755b674e6f56581094d847993103412216
wagayaluda <401576148@qq.com>
2019-03-30 11:56:28 +0800

    2019.3.30 11:55 QQ绑定验证功能细节修改

--- Services/Login/Authorization.cs
+++ Services/Login/Authorization.cs
@@ -64,18 +64,21 @@ namespace ServerSideCharacter2.Services.Login
 				if (serverPlayer.HasPassword)
 				{
                     //以下为QQ绑定验证代码
+                    //bool isAuthSuccess 为验证状态
+                    //string QQ 为用户绑定的QQ号（如果已绑定）
                     bool isAuthSuccess = false;
                     string ClientMD5Key = "This is the CLIENT key.";
                     string ServerMD5Key = "This is the SERVER key.";
                     string username = info.UserName;
                     char[] constant =
                     {
-                '0','1','2','3','4','5','6','7','8','9',
-                'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
-                'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
-            };
+                        '0','1','2','3','4','5','6','7','8','9',
+                        'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+                        'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
+                    };
                     System.Text.StringBuilder newRandom = new System.Text.StringBuilder(62);
                     System.Random rd = new System.Random();
+                    //salt暂定长度为10
                     for (int i = 0; i < 10; i++)
                     {
                         newRandom.Append(constant[rd.Next(62)]);
@@ -98,12 +101,12 @@ namespace ServerSideCharacter2.Services.Login
                             Credentials = System.Net.CredentialCache.DefaultCredentials
                         }.DownloadData(string.Concat(new string[]
                         {
-                    "http://peserver.terrariaserver.cn/SteamCityAuth.aspx?username=",
-                    username,
-                    "&salt=",
-                    salt,
-                    "&check=",
-                    check
+                            "http://peserver.terrariaserver.cn/SteamCityAuth.aspx?username=",
+                            username,
+                            "&salt=",
+                            salt,
+                            "&check=",
+                            check
                         })));
                         string _state = pageData.Substring(pageData.IndexOf("<span id=\"statelabel\">") + 22, pageData.IndexOf("</span>", pageData.IndexOf("<span id=\"statelabel\">")) - pageData.IndexOf("<span id=\"statelabel\">") - 22);
                         string _check = pageData.Substring(pageData.IndexOf("<span id=\"checklabel\">") + 22, pageData.IndexOf("</span>", pageData.IndexOf("<span id=\"checklabel\">")) - pageData.IndexOf("<span id=\"checklabel\">") - 22);
