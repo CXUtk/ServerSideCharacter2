b048d79ab554b4d247c4ec7b2f11f9574e6950e3
DXTsT <2237367067@qq.com>
2019-03-31 19:28:14 +0800

    DEBUG模式去掉

--- ServerSideCharacter2.cs
+++ ServerSideCharacter2.cs
@@ -32,7 +32,7 @@ namespace ServerSideCharacter2
 {
 	public class ServerSideCharacter2 : Mod
 	{
-		public const bool DEBUGMODE = true;
+		public const bool DEBUGMODE = false;
 
 		internal static ServerSideCharacter2 Instance;
 
